32bfc16750fe19ded562a0034a4bdcfa536f0934
justinhu704 <82266917+justinhu704@users.noreply.github.com>
2022-02-23 17:00:08 +0800

    Update Web.py

--- Web.py
+++ Web.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,15 +5,3 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('Meteor.html')
-
-=======
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('Meteor.html')
-
->>>>>>> 00a058feb37054fd60e37b3569dfe0169549379a
-app.run(port = 3000)
